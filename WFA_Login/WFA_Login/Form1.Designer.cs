56946a2c43a744d7a268bc6a8f5fa519a0da4f72
Ahmet Erden <erden.hmt@gmail.com>
2020-03-06 20:35:10 +0300

    new object added
    WFA_SignIn added.

---
+++ WFA_Login/WFA_Login/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace WFA_Login
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnSignIn = new System.Windows.Forms.Button();
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblRemaining = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblCharacter = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnSignIn
+            // 
+            this.btnSignIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSignIn.Location = new System.Drawing.Point(379, 225);
+            this.btnSignIn.Name = "btnSignIn";
+            this.btnSignIn.Size = new System.Drawing.Size(236, 51);
+            this.btnSignIn.TabIndex = 9;
+            this.btnSignIn.Text = "Sign In";
+            this.btnSignIn.UseVisualStyleBackColor = true;
+            this.btnSignIn.Click += new System.EventHandler(this.btnSignIn_Click);
+            // 
+            // txtValue
+            // 
+            this.txtValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtValue.Location = new System.Drawing.Point(24, 225);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(349, 41);
+            this.txtValue.TabIndex = 8;
+            this.txtValue.TextChanged += new System.EventHandler(this.txtValue_TextChanged);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 34F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTime.Location = new System.Drawing.Point(657, 213);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(92, 65);
+            this.lblTime.TabIndex = 3;
+            this.lblTime.Text = "10";
+            // 
+            // lblRemaining
+            // 
+            this.lblRemaining.AutoSize = true;
+            this.lblRemaining.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblRemaining.Location = new System.Drawing.Point(375, 291);
+            this.lblRemaining.Name = "lblRemaining";
+            this.lblRemaining.Size = new System.Drawing.Size(36, 39);
+            this.lblRemaining.TabIndex = 4;
+            this.lblRemaining.Text = "5";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(20, 291);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Remaining:";
+            // 
+            // lblCharacter
+            // 
+            this.lblCharacter.AutoSize = true;
+            this.lblCharacter.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCharacter.ForeColor = System.Drawing.Color.Maroon;
+            this.lblCharacter.Location = new System.Drawing.Point(19, 170);
+            this.lblCharacter.Name = "lblCharacter";
+            this.lblCharacter.Size = new System.Drawing.Size(79, 36);
+            this.lblCharacter.TabIndex = 6;
+            this.lblCharacter.Text = "........";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(19, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(524, 29);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "type the following characters in the relevant field";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSignIn);
+            this.Controls.Add(this.txtValue);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.lblRemaining);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblCharacter);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSignIn;
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblRemaining;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblCharacter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WFA_Login/WFA_Login/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA_Login
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random random = new Random();
+        int character;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            character= random.Next(1000000, 10000000);
+            lblCharacter.Text = character.ToString();
+            timer1.Start();
+                
+
+        }
+
+        private void btnSignIn_Click(object sender, EventArgs e)
+        {
+            if (txtValue.Text.Length>0)
+            {
+                if (Convert.ToInt32(lblCharacter.Text) == Convert.ToInt32(txtValue.Text))
+                {
+                    MessageBox.Show("Successful Login");
+                }
+                else
+                {
+                    if (lblRemaining.Text != "0")
+                    {
+                        int remainingInput = Convert.ToInt32(lblRemaining.Text);
+                        remainingInput--;
+                        lblRemaining.Text = remainingInput.ToString();
+                        character = random.Next(1000000, 10000000);
+                        lblCharacter.Text = character.ToString();
+                    }
+                  else
+                    {
+                        MessageBox.Show("you can't try more");
+                        btnSignIn.Enabled = false;
+                        txtValue.Enabled = false;
+
+                    }
+
+                }
+            }
+
+            else
+            {
+                MessageBox.Show("please define at least one value");
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int duration = Convert.ToInt32(lblTime.Text);
+            duration--;
+            lblTime.Text = duration.ToString();
+            if (duration == 0)
+            {
+               
+                btnSignIn.Enabled = false;
+                txtValue.Enabled = false;
+                timer1.Stop();
+            }
+            else if (duration<=5)
+            {
+                btnSignIn.BackColor = Color.Red;
+
+
+
+            }
+
+            
+
+        }
+
+        private void txtValue_TextChanged(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            if (txtValue.Text.Length==0)
+            {
+
+                timer1.Start();
+            }
+        }
+    }
+}
